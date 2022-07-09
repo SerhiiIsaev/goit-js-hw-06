24b66566397a51d451834c4f2da95fceef791684
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-10 00:17:09 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+
+const arrayOfElement = []
+
+for (const ingredient of ingredients) {
+const item = document.createElement('li')  
+  item.textContent = ingredient
+  item.classList.add('item')
+  arrayOfElement.push(item)
+}
+
+
+ingredientsList.append(...arrayOfElement)
