15a550cf35cda9bd459bf4c23ae4de324ba3ad49
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-11 21:18:58 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const textColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('button');
+
+changeColorBtn.addEventListener('click', onBodyChangeColor);
+
+function onBodyChangeColor(event) {
+  textColor.textContent = getRandomHexColor();
+  body.style.color = textColor.textContent;
+}
