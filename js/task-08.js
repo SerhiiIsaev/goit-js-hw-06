ee085f3661e708d610ad23b9ab5ca743114a1b34
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-11 20:54:01 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    const user = {
+        email: event.currentTarget.elements.email.value,
+        password: event.currentTarget.elements.password.value,
+    }
+
+    
+
+    console.log(user)   
+    event.currentTarget.reset()
+}
